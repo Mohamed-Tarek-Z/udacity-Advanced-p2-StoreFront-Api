bcc1d8d1b434c2d9a47baaade1574e42e0d6fd2b
Mohamed-Tarek-Z <MohamedTarek153@gmail.com>
2022-07-13 16:45:50 +0200

    after sht

--- migrations/test/sqls/20220711032550-orders-up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE orders(id SERIAL PRIMARY KEY, user_id BIGINT REFERENCES users(id), status VARCHAR(10) CHECK(status = 'active' OR status ='complete'));
--- migrations/test/sqls/20220711032617-order-product-up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE order_product(id SERIAL PRIMARY KEY, order_id BIGINT REFERENCES orders(id), product_id BIGINT REFERENCES products(id), quantity INT NOT NULL);
